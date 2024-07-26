b0da8c4983c658be7210c0334c76aaaeee941bc5
pieceowater <pieceowater@gmail.com>
2024-07-26 23:00:29 +0500

    new bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,13 @@
 // src/main.ts
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
-import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
+import { bootstrap } from '@pieceowater-dev/lotof.lib.broadcaster';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-
-  // noinspection TypeScriptValidateTypes
-  const rabbitMQUrl = configService.get<string>('rabbitUrl');
-  // noinspection TypeScriptValidateTypes
-  const port = configService.get<number>('port') || 3000;
-
-  app.connectMicroservice<MicroserviceOptions>({
+bootstrap(AppModule, {
+  portEnvVar: 'PORT',
+  microservices: [{
     transport: Transport.RMQ,
-    options: {
-      urls: [rabbitMQUrl],
-      queue: 'template_queue',
-      queueOptions: {
-        durable: false,
-      },
-    },
-  });
-
-  await app.startAllMicroservices();
-  await app.listen(port);
-  console.log(`Application is running on: ${await app.getUrl()}`);
-}
-bootstrap();
+    urlEnvVars: ['RABBITMQ_URL'],
+    queue: 'template_queue'
+  }]
+}).then(r => console.log('ok'))
