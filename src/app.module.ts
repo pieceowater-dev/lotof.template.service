6f16f3dcbc06e0502adfa6a18f81666e6e6d0668
pieceowater <pieceowater@gmail.com>
2024-07-28 13:31:04 +0500

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
-// src/main.ts
-import { AppModule } from './app.module';
-import { Transport } from '@nestjs/microservices';
-import { bootstrap } from '@pieceowater-dev/lotof.lib.broadcaster';
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigAppModule } from './core/config/config.module';
+import { ItemModule } from './modules/item/item.module';
+import { DatabaseModule } from './core/database/database.module';
+import { HealthModule } from './modules/health/health.module';
 
-bootstrap(AppModule, {
-  portEnvVar: 'PORT',
-  microservices: [
-    {
-      transport: Transport.RMQ,
-      urlEnvVars: ['RABBITMQ_URL'],
-      queue: 'template_queue',
-    },
+@Module({
+  imports: [
+    DatabaseModule,
+    ConfigAppModule,
+    HealthModule,
+    ItemModule,
   ],
-}).then((r) => console.log('Booted successfully 🚀'));
+})
+export class AppModule {}
